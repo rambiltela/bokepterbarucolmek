667a6b151a20fd289475172c621104d66996bd09
rambiltela <rambiltela@gmail.com>
2025-05-17 12:25:37 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bokepterbarucolmek
-bokepterbarucolmek
+[https://bokepterbarucolmek.pages.dev](https://bokepterbarucolmek.pages.dev/)
+Fork the [repository](https://github.com/somisaldo7) on GitHub.
---
+++ app/layout.tsx
@@ -0,0 +1,82 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+    metadataBase: new URL("https://bokepterbarucolmek.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'K6R_-obTWUVrCSvb99WInvoAhX96XMTn2Ys3o8pcg0U',
+        yandex: '5b04b224bfd83b24',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        "logo": "https://bokepterbarucolmek.pages.dev/favicon.ico",
+        "url": "https://bokepterbarucolmek.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://bokepterbarucolmek.pages.dev"
+            },
+            "foundingDate":"2025-05-16"
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://bokepterbarucolmek.pages.dev",
+        "description": `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://bokepterbarucolmek.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
