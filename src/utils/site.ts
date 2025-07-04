abe86f0257fe58937c0931ac34599329c9aba14d
rambiltela <rambiltela@gmail.com>
2025-07-04 09:14:16 +0700

    Add files via upload

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,6 +1,2 @@
-export const nama = 'Bokep Terbaru Colmek';
-export const desk = 'Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma';
-export const url = 'https://bokepterbarucolmek.pages.dev';
-export const terbit = '2025-05-16T00:21:03Z';
 export const google= 'K6R_-obTWUVrCSvb99WInvoAhX96XMTn2Ys3o8pcg0U'
 export const yandex= '5b04b224bfd83b24'
